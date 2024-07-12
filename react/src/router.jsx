9b3bb9c63c33e6615b99f235859d96d6ac4770c3
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-07-12 10:40:19 +0530

    configure react router

---
+++ react/src/router.jsx
@@ -0,0 +1,26 @@
+import { createBrowserRouter } from 'react-router-dom'
+import Login from './views/Login'
+import Users from './views/Users'
+import Signup from './views/Signup'
+import NotFound from './views/NotFound'
+
+const router = createBrowserRouter([
+    {
+        path: '/login',
+        element: <Login />
+    },
+    {
+        path: '/signup',
+        element: <Signup />
+    },
+    {
+        path: '/users',
+        element: <Users />
+    },
+    {
+        path: '*',
+        element: <NotFound />
+    }
+])
+
+export default router
