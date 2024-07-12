4aa6d3e98ce47c307ed7037668ea915364347d3c
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-07-12 19:11:01 +0530

    create users and update users in frontend

--- react/src/views/UserForm.jsx
+++ react/src/views/UserForm.jsx
@@ -1,5 +1,89 @@
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import axiosClient from "../axios-client";
+
 export default function UserForm() {
-  return (
-    <div>UserForm</div>
-  )
+    const navigate = useNavigate();
+    let { id } = useParams();
+
+    const [user, setUser] = useState({
+        id: null,
+        name: '',
+        email: '',
+        password: '',
+        password_confirmation: ''
+    })
+    const [loading, setLoading] = useState(false)
+    const [errors, setErrors] = useState(null)
+
+    if (id) {
+        useEffect(() => {
+            setLoading(true)
+            axiosClient.get(`/users/${id}`)
+                .then(({ data }) => {
+                    setLoading(false)
+                    setUser(data)
+                })
+                .catch(() => {
+                    setLoading(false)
+                })
+        }, [])
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+        if (user.id) {
+            axiosClient.put(`/users/${user.id}`, user)
+                .then(() => {
+                    navigate('/users')
+                })
+                .catch(err => {
+                    const response = err.response;
+                    if (response && response.status === 422) {
+                        setErrors(response.data.errors)
+                    }
+                })
+        } else {
+            axiosClient.post('/users', user)
+                .then(() => {
+                    navigate('/users')
+                })
+                .catch(err => {
+                    const response = err.response;
+                    if (response && response.status === 422) {
+                        setErrors(response.data.errors)
+                    }
+                })
+        }
+    }
+
+    return (
+        <>
+            {user.id && <h1>Update User: {user.name}</h1>}
+            {!user.id && <h1>New User</h1>}
+            <div className="card animated fadeInDown">
+                {loading && (
+                    <div className="text-center">
+                        Loading...
+                    </div>
+                )}
+                {errors &&
+                    <div className="alert">
+                        {Object.keys(errors).map(key => (
+                            <p key={key}>{errors[key][0]}</p>
+                        ))}
+                    </div>
+                }
+                {!loading && (
+                    <form onSubmit={onSubmit}>
+                        <input value={user.name} onChange={(e) => setUser({ ...user, name: e.target.value })} placeholder="Name" />
+                        <input value={user.email} onChange={(e) => setUser({ ...user, email: e.target.value })} placeholder="Email" />
+                        <input type="password" onChange={(e) => setUser({ ...user, password: e.target.value })} placeholder="Password" />
+                        <input type="password" onChange={(e) => setUser({ ...user, password_confirmation: e.target.value })} placeholder="Password Confirmation" />
+                        <button className="btn">Save</button>
+                    </form>
+                )}
+            </div>
+        </>
+    )
 }
