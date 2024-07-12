d95dbb7c5d8094cbf60a601f8002f438d2f81117
Lasindu Maduranga <lasindumvp2001@gmail.com>
2024-07-12 15:39:11 +0530

    users crud in backend

--- react/src/views/Users.jsx
+++ react/src/views/Users.jsx
@@ -1,5 +1,46 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axiosClient from "../axios-client";
+
 export default function Users() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    getUsers();
+  }, [])
+
+  const getUsers = () => {
+    setLoading(true)
+    axiosClient.get('/users')
+      .then(({ data }) => {
+        setLoading(false)
+        setUsers(data.data)
+      })
+      .catch(() => {
+        setLoading(false)
+      })
+  }
+
   return (
-    <div>Users</div>
+    <div>
+      <div style={{ display: 'flex', justifyContent: "space-between", alignItems: "center" }}>
+        <h1>Users</h1>
+        <Link className="btn-add" to="/users/new">Add new</Link>
+      </div>
+      <div className="card animated fadeInDown">
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Create Date</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+        </table>
+      </div>
+    </div>
   )
 }
